f516bec4ad3a6a11d1be5e780acbbd9a47bfff6f
Kakaranish <stasiekgruz@gmail.com>
2020-12-18 09:59:20 +0100

    + make solution building

--- Identity.API/Extensions/StartupExtensions.cs
+++ Identity.API/Extensions/StartupExtensions.cs
@@ -1,20 +1,12 @@
 using Common.Authentication;
-using Identity.API.DataAccess;
 using Identity.API.DataAccess.Repositories;
 using Identity.API.Services;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Identity.API.Extensions
 {
     public static class StartupExtensions
     {
-        public static void SeedTestDb(this IApplicationBuilder applicationBuilder)
-        {
-            var dbInitializer = applicationBuilder.ApplicationServices.GetRequiredService<IDbInitializer>();
-            dbInitializer.SeedData();
-        }
-
         public static IServiceCollection AddInternalServices(this IServiceCollection services)
         {
             services.AddSingleton<PasswordValidatorBase, PasswordValidator>();
@@ -28,8 +20,6 @@ namespace Identity.API.Extensions
 
             services.AddSingleton<IAccessTokenDecoder, AccessTokenDecoder>();
             services.AddSingleton<IRefreshTokenDecoder, RefreshTokenDecoder>();
-
-            services.AddScoped<IUserService, UserService>();
 
             return services;
         }
--- Products.API/Controllers/ProductsController.cs
+++ Products.API/Controllers/ProductsController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Products.API.DataAccess.Repositories;
 using Products.API.Domain;
+using Products.API.Dto;
+using System;
+using System.Threading.Tasks;
 
 namespace Products.API.Controllers
 {
