95b4d25acda30e1a73ac664b69be23f16507be38
Kakaranish <stasiekgruz@gmail.com>
2021-03-04 19:33:30 +0100

    + extended logging in OfferBecameUnavaialbleIntegrationEventHandler

--- Offers.API/Application/DomainEvents/OfferBecameUnavailable/OfferBecameUnavailableDomainEventHandler.cs
+++ Offers.API/Application/DomainEvents/OfferBecameUnavailable/OfferBecameUnavailableDomainEventHandler.cs
@@ -1,6 +1,9 @@
 ﻿using Common.Domain;
 using Common.EventBus;
 using Common.EventBus.IntegrationEvents;
+using Common.Extensions;
+using Common.Logging;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,18 +12,30 @@ namespace Offers.API.Application.DomainEvents.OfferBecameUnavailable
 {
     public class OfferBecameUnavailableDomainEventHandler : IDomainEventHandler<OfferBecameUnavailableDomainEvent>
     {
+        private readonly ILogger<OfferBecameUnavailableDomainEventHandler> _logger;
         private readonly IEventBus _eventBus;
 
-        public OfferBecameUnavailableDomainEventHandler(IEventBus eventBus)
+        public OfferBecameUnavailableDomainEventHandler(ILogger<OfferBecameUnavailableDomainEventHandler> logger,
+            IEventBus eventBus)
         {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
         }
 
         public async Task Handle(OfferBecameUnavailableDomainEvent domainEvent, CancellationToken cancellationToken)
         {
+            _logger.LogWithProps(LogLevel.Information,
+                $"Handling {nameof(OfferBecameUnavailableDomainEvent)} domain event",
+                "OfferId".ToKvp(domainEvent.OfferId));
+
             var integrationEvent = new OfferBecameUnavailableIntegrationEvent { OfferId = domainEvent.OfferId };
 
             await _eventBus.PublishAsync(integrationEvent);
+
+            _logger.LogWithProps(LogLevel.Information,
+                $"Integration event {nameof(OfferBecameUnavailableIntegrationEvent)} published",
+                "EventId".ToKvp(integrationEvent.Id),
+                "OfferId".ToKvp(domainEvent.OfferId));
         }
     }
 }
