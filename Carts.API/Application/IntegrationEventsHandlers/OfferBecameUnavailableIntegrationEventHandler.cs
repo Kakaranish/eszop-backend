b213c9fc5fb535f92ca1ddc4681a30f6b05da3b4
Kakaranish <stasiekgruz@gmail.com>
2021-03-04 20:30:02 +0100

    + extended logging in integration handler

--- Carts.API/Application/IntegrationEventsHandlers/OfferBecameUnavailableIntegrationEventHandler.cs
+++ Carts.API/Application/IntegrationEventsHandlers/OfferBecameUnavailableIntegrationEventHandler.cs
@@ -1,6 +1,9 @@
 ﻿using Carts.API.DataAccess.Repositories;
 using Common.EventBus;
 using Common.EventBus.IntegrationEvents;
+using Common.Extensions;
+using Common.Logging;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -8,17 +11,30 @@ namespace Carts.API.Application.IntegrationEventsHandlers
 {
     public class OfferBecameUnavailableIntegrationEventHandler : IntegrationEventHandler<OfferBecameUnavailableIntegrationEvent>
     {
+        private readonly ILogger<OfferBecameUnavailableIntegrationEventHandler> _logger;
         private readonly ICartItemRepository _cartItemRepository;
 
-        public OfferBecameUnavailableIntegrationEventHandler(ICartItemRepository cartItemRepository)
+        public OfferBecameUnavailableIntegrationEventHandler(
+            ILogger<OfferBecameUnavailableIntegrationEventHandler> logger, ICartItemRepository cartItemRepository)
         {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _cartItemRepository = cartItemRepository ?? throw new ArgumentNullException(nameof(cartItemRepository));
         }
 
         public override async Task Handle(OfferBecameUnavailableIntegrationEvent @event)
         {
-            _cartItemRepository.RemoveWithOfferId(@event.OfferId);
+            _logger.LogWithProps(LogLevel.Information,
+                $"Handling {nameof(OfferBecameUnavailableIntegrationEvent)} integration event",
+                "EventId".ToKvp(@event.Id),
+                "OfferId".ToKvp(@event.OfferId));
+
+            _cartItemRepository.RemoveWithOfferId(@event.OfferId); //TODO? Notify user
             await _cartItemRepository.UnitOfWork.SaveChangesAndDispatchDomainEventsAsync();
+
+            _logger.LogWithProps(LogLevel.Information,
+                $"Handled {nameof(OfferBecameUnavailableIntegrationEvent)} integration event",
+                "EventId".ToKvp(@event.Id),
+                "OfferId".ToKvp(@event.OfferId));
         }
     }
 }
