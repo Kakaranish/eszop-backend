769cdad38e4b43b2f233924ba514069ba06a6de4
Kakaranish <stasiekgruz@gmail.com>
2021-03-27 21:27:10 +0100

    + changed start-services script + changed scripts dir structure

--- scripts/start-services.ps1
+++ scripts/start-services.ps1
@@ -1,18 +1,18 @@
-$lookup_dirs = @(
-    "..\API.Gateway",
-    "..\Carts.API",
-    "..\Identity.API",
-    "..\NotificationService",
-    "..\Offers.API",
-    "..\Orders.API" 
-);
+param(
+    [string] $ImageTag = "latest"
+)
 
-foreach ($dir in $lookup_dirs) {
-    $scripts_dir = Join-Path (Resolve-Path $dir) "scripts"
-    $build_script = Join-Path $scripts_dir "start-service.ps1"
+Import-Module $PSScriptRoot\modules\Resolve-ServiceLocation.psm1 -Force
 
-    if(Test-Path $build_script) {
-        Write-Host "Run $build_script"
-        & $build_script
+$services = @("gateway", "carts", "identity", "notification", "offers", "orders")
+
+foreach ($service in $services) {
+    $service_dir = Resolve-ServiceLocation -ServiceName $service
+    $scripts_dir = Join-Path (Resolve-Path $service_dir) "scripts"
+    $start_script = Join-Path $scripts_dir "start-service.ps1"
+
+    if(Test-Path $start_script) {
+        Write-Host "Run $start_script"
+        & $start_script -ImageTag $ImageTag
     }
 }
