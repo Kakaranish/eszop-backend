5147d67e2532ef06f4baa4db12e527e0deb7bcbe
Kakaranish <stasiekgruz@gmail.com>
2021-03-27 13:47:32 +0100

    + created setup scripts for: redis, conn strings, azure storage

---
+++ scripts/setup-scripts/set-sqlserver-connection-strings.ps1
@@ -0,0 +1,59 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $DbUsername,
+
+    [Parameter(Mandatory = $true)]
+    [string] $DbPassword
+)
+
+Import-Module $PSScriptRoot\..\modules\Resolve-EnvPrefix.psm1 -Force
+Import-Module $PSScriptRoot\..\modules\Resolve-ServiceLocation.psm1 -Force
+
+if (-not($ConnectionString)) {
+    $ConnectionString = $default_connection_string
+}
+
+$services = @("carts", "identity", "notification", "offers", "orders")
+
+$environment = $env:ASPNETCORE_ENVIRONMENT
+if (-not($environment)) {
+    $environment = "DevelopmentLocal"
+}
+
+$environment_prefix = Resolve-EnvPrefix -Environment $environment
+$appsettings_filename = "appsettings.$environment.json"
+
+$connection_string_template = "Server=tcp:eszop-{env_prefix}-{service}-sqlserver.database.windows.net,1433;Initial Catalog=eszop;Persist Security Info=False;User ID={db_username};Password={db_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+# ------------------------------------------------------------------------------
+
+Write-Host "Those files will be changed:"
+foreach ($service in $services) {
+    $service_location = Resolve-ServiceLocation -ServiceName $service
+    Write-Host (Resolve-Path -Path (Join-Path $service_location $appsettings_filename))
+}
+$choice = Read-Host "Do you want to continue (y/n)"
+if ($choice -ne "y") {
+    exit
+}
+
+foreach ($service in $services) {
+    $service_location = Resolve-ServiceLocation -ServiceName $service
+    $appsettings_path = Resolve-Path -Path (Join-Path $service_location $appsettings_filename)
+    if (-not(Test-Path -Path $appsettings_path)) {
+        Write-Warning "[SKIP] File $appsettings_path does not exist"
+        continue
+    }
+
+    $connection_string = $connection_string_template `
+        -replace "{env_prefix}", $environment_prefix `
+        -replace "{service}", $service `
+        -replace "{db_username}", $DbUsername `
+        -replace "{db_password}", $DbPassword
+
+    $appsettings_json = Get-Content -Path $appsettings_path | ConvertFrom-Json
+    $appsettings_json.ConnectionStrings | Add-Member -Name "SqlServer" -MemberType NoteProperty $connection_string -Force
+    $appsettings_json | ConvertTo-Json -Depth 9 | Set-Content $appsettings_path
+}
+
+Write-Host "appsettings.json files updated successfully"
---
+++ scripts/setup-scripts/set-storage.ps1
@@ -0,0 +1,46 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $ConnectionString
+)
+
+Import-Module $PSScriptRoot\..\modules\Resolve-ServiceLocation.psm1 -Force
+Import-Module $PSScriptRoot\..\modules\Resolve-EnvPrefix.psm1 -Force
+
+$environment = $env:ASPNETCORE_ENVIRONMENT
+if (-not($environment)) {
+    $environment = "DevelopmentLocal"
+}
+
+$service = "offers"
+$environment_prefix = Resolve-EnvPrefix -Environment $environment
+
+$appsettings_filename = "appsettings.$environment.json"
+$service_location = Resolve-ServiceLocation -ServiceName $service
+$appsettings_path = Resolve-Path -Path (Join-Path $service_location $appsettings_filename)
+
+#-------------------------------------------------------------------------------
+
+Write-Host "This file will be changed:"
+Write-Host $appsettings_path
+$choice = Read-Host "Do you want to continue (y/n)"
+if ($choice -ne "y") {
+    exit
+}
+
+if (-not(Test-Path -Path $appsettings_path)) {
+    Write-Warning "[SKIP] File $appsettings_path does not exist"
+    continue
+}
+
+$storage_section = @"
+{
+    "ConnectionString": "$ConnectionString",
+    "ContainerName": "eszop-$environment_prefix-storage-container"
+}
+"@
+
+$appsettings_json = Get-Content -Path $appsettings_path | ConvertFrom-Json
+$appsettings_json | Add-Member -Name "AzureStorage" -MemberType NoteProperty -Value (ConvertFrom-Json $storage_section) -Force
+$appsettings_json | ConvertTo-Json -Depth 9 | Set-Content $appsettings_path
+
+Write-Host "$appsettings_path updated successfully"
