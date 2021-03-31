5e59209786be728ba81ec789bec27e8093def6d7
Kakaranish <stasiekgruz@gmail.com>
2021-03-31 19:38:28 +0200

    + created setup-DevHostCloud-env

---
+++ scripts/setup-scripts/setup-DevHostCloud-env.ps1
@@ -0,0 +1,67 @@
+param(
+    [switch] $AutoApprove
+)
+
+Import-Module $PSScriptRoot\..\modules\Resolve-ServiceLocation.psm1 -Force
+
+# ---  CONFIG  -----------------------------------------------------------------
+
+$ESZOP_AZURE_STORAGE_CONN_STR = ""
+$ESZOP_AZURE_EVENTBUS_CONN_STR = ""
+$ESZOP_REDIS_CONN_STR = ""
+
+$db_username = ""
+$db_password = ""
+$ESZOP_SQLSERVER_CONN_STR_template = ""
+
+$cloud_env_prefix = "staging"
+
+# ------------------------------------------------------------------------------
+
+$services = @("carts", "identity", "notification", "offers", "orders")
+
+if (-not($AutoApprove.IsPresent)) {
+    Write-Host "Those files will be changed:"
+    foreach ($service in $services) {
+        $service_location = Resolve-ServiceLocation -ServiceName $service
+        Write-Host (Resolve-Path -Path (Join-Path $service_location "Properties" "launchSettings.json"))
+    }
+    $choice = Read-Host "Do you want to continue (y/n)"
+    if ($choice -ne "y") {
+        exit
+    }
+}
+
+foreach ($service in $services) {
+    $service_location = Resolve-ServiceLocation -ServiceName $service
+    $launch_settings_path = Resolve-Path -Path (Join-Path $service_location "Properties" "launchSettings.json")
+    if (-not(Test-Path -Path $launch_settings_path)) {
+        Write-Warning "[SKIP] File $launch_settings_path does not exist"
+        continue
+    }
+
+    $env_variables_to_set = @{
+        "ESZOP_AZURE_STORAGE_CONN_STR"  = $ESZOP_AZURE_STORAGE_CONN_STR;
+        "ESZOP_AZURE_EVENTBUS_CONN_STR" = $ESZOP_AZURE_EVENTBUS_CONN_STR;
+        "ESZOP_REDIS_CONN_STR"          = $ESZOP_REDIS_CONN_STR;
+        "ESZOP_SQLSERVER_CONN_STR"      = $ESZOP_SQLSERVER_CONN_STR_template `
+            -replace "{env_prefix}", $cloud_env_prefix `
+            -replace "{service_name}", $service `
+            -replace "{db_username}", $db_username `
+            -replace "{db_password}", $db_password;
+    }
+
+    $launch_settings_json = Get-Content -Path $launch_settings_path | ConvertFrom-Json -Depth 9
+    
+    $cloud_profile_name = ($launch_settings_json.profiles.PSObject.Properties | Where-Object { $_.Name -match "Cloud" })[0].Name
+    $profile_env_vars = $launch_settings_json.profiles.$cloud_profile_name.environmentVariables
+
+    foreach ($env_variable_to_set in $env_variables_to_set.Keys) {
+        if (($profile_env_vars.PSObject.Properties | Where-Object { $_.Name -eq $env_variable_to_set }).Count) {
+            $val_to_set = $env_variables_to_set.$env_variable_to_set
+            $launch_settings_json.profiles.$cloud_profile_name.environmentVariables | `
+                Add-Member -Name $env_variable_to_set -MemberType NoteProperty -Value $val_to_set -Force
+        }
+    }
+    $launch_settings_json | ConvertTo-Json -Depth 9 | Set-Content $launch_settings_path
+}
