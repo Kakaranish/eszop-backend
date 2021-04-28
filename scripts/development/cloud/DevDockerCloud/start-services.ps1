5d8e73860767bcc8a433e4c2f73dea5f6980159a
Kakaranish <stasiekgruz@gmail.com>
2021-04-28 20:14:03 +0200

    + commit case sensitive files...

--- scripts/development/cloud/DevDockerCloud/start-services.ps1
+++ scripts/development/cloud/DevDockerCloud/start-services.ps1
@@ -1,46 +1,49 @@
 param(
-    [string] $ImageTag = "latest",
+  [string] $ImageTag = "latest",
 
-    [Parameter(Mandatory = $true)]    
-    [ValidateSet("DevCloud", "Staging")]
-    [string] $TargetCloudEnv = "Staging",
-
-    [switch] $SetUpEnvironmentVariables
-)
-$modules_dir = "$PSScriptRoot\..\..\..\modules"
-Import-Module "${modules_dir}\Resolve-ServiceLocation.psm1" -Force
-Import-Module "${modules_dir}\Require-EnvironmentVariables.psm1" -Force -DisableNameChecking
-Import-Module "${modules_dir}\Resolve-EnvPrefix.psm1" -Force -DisableNameChecking
-
-if($SetUpEnvironmentVariables.IsPresent) {
-    & "$PSScriptRoot\set-up-DevDockerCloud-env.ps1"
-}
-
-$required_env_variables = @(
-    "ASPNETCORE_ENVIRONMENT",
-    "ESZOP_AZURE_EVENTBUS_CONN_STR",
-    "ESZOP_REDIS_CONN_STR",
-    "ESZOP_AZURE_STORAGE_CONN_STR",
-    "ESZOP_SQLSERVER_CONN_STR_OFFERS",
-    "ESZOP_SQLSERVER_CONN_STR_IDENTITY",
-    "ESZOP_SQLSERVER_CONN_STR_CARTS",
-    "ESZOP_SQLSERVER_CONN_STR_ORDERS",
-    "ESZOP_SQLSERVER_CONN_STR_NOTIFICATION"
+  [Parameter(Mandatory = $true)]
+  [ValidateSet("dev", "staging", "prod")] 
+  [string] $CloudEnv
 )
 
-$target_cloud_env_prefix = Resolve-EnvPrefix -Environment $TargetCloudEnv
+$scripts_dir = "$PSScriptRoot\..\..\.."
+Import-Module "${scripts_dir}\modules\Resolve-ServiceLocation.psm1" -Force
+Import-Module "${scripts_dir}\modules\Get-MultipleEnvVariables.psm1" -Force -DisableNameChecking
+Import-Module "${scripts_dir}\modules\Set-MultipleEnvVariables.psm1" -Force -DisableNameChecking
 
-Require-EnvironmentVariables -EnvironmentVariables $required_env_variables
+# ------------------------------------------------------------------------------
+
+$env_vars_to_backup = @(
+  "ESZOP_CLIENT_URI",
+  "ESZOP_AZURE_EVENTBUS_CONN_STR",
+  "ESZOP_AZURE_STORAGE_CONN_STR",
+  "ESZOP_REDIS_CONN_STR",
+  "ESZOP_SQLSERVER_CONN_STR_OFFERS",
+  "ESZOP_SQLSERVER_CONN_STR_IDENTITY",
+  "ESZOP_SQLSERVER_CONN_STR_CARTS",
+  "ESZOP_SQLSERVER_CONN_STR_ORDERS",
+  "ESZOP_SQLSERVER_CONN_STR_NOTIFICATION",
+  "ESZOP_SQLSERVER_CONN_STR",
+  "ASPNETCORE_ENVIRONMENT"
+)
+
+$env_vars_backup = Get-MultipleEnvVariables -Variables $env_vars_to_backup
+
+$env:ASPNETCORE_ENVIRONMENT = "DevDockerCloud"
+. "${scripts_dir}\setup-scripts\Set-AppEnvVariables.ps1" -CloudEnv $CloudEnv
+
+Write-Host $env:ESZOP_CLIENT_URI
 
 $services = @("gateway", "carts", "identity", "notification", "offers", "orders")
-
 foreach ($service in $services) {
-    $service_dir = Resolve-ServiceLocation -ServiceName $service
-    $scripts_dir = Join-Path (Resolve-Path $service_dir) "scripts" "docker" "DevDockerCloud"
-    $start_script = Join-Path $scripts_dir "start-service.ps1"
+  $service_dir = Resolve-ServiceLocation -ServiceName $service
+  $scripts_dir = Join-Path (Resolve-Path $service_dir) "scripts" "docker" "DevDockerCloud"
+  $start_script = Join-Path $scripts_dir "start-service.ps1"
 
-    if(Test-Path $start_script) {
-        Write-Host "Run $start_script"
-        & $start_script -ImageTag $ImageTag -TargetCloudEnvPrefix $target_cloud_env_prefix
-    }
+  if (Test-Path $start_script) {
+    Write-Host "Run $start_script"
+    . $start_script -ImageTag $ImageTag -TargetCloudEnvPrefix $CloudEnv
+  }
 }
+
+Set-MultipleEnvVariables -EnvDictionary $env_vars_backup
